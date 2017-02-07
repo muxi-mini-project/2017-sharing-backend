edec61769840d576fa5c6dcc97faa0926933273a
bofan li <1105388289@qq.com>
2017-02-07 23:27:29 +0800

    用户登录

--- sharing/app/auth/forms.py
+++ sharing/app/auth/forms.py
@@ -1,11 +1,32 @@
 #coding:utf-8
 from flask_wtf import Form
 from wtforms import StringField,PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, Rexgexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(Form):
-    email = StringField('邮箱地址', validators=[Required(), Length(1,64), 
+    email = StringField(u'账号', validators=[Required(), Length(1,64), 
                                                 Email()])
-    password = PasswordField('密码', validators=[Required()])
-    remeber_me = BooleanField('记住我')
-    submit = SubmitField('登录')
+    password = PasswordField(u'密码', validators=[Required()])
+    remeber_me = BooleanField(u'记住我')
+    submit = SubmitField(u'登录')
+
+class RegistrationForm(Form):
+    email = StringField(u"账号", validators=[Required(), Length(1,64),
+                                            Email()])
+    username = StringField(u"昵称", validators=[
+        Required(), Length(6,64), Rexgexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+            u'用户名必须以字母开头')])
+    password = PasswordField(u'密码', validators=[
+        Required(), Length(8,16),EquelTo('password2', message=u'密码必须匹配')])
+    password2 = PasswordField(u'请再次输入密码', validators=[Required()])
+    submit = Submit(u'注册')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(u'邮箱已被注册')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError(u'用户名已被注册')
--- sharing/app/auth/views.py
+++ sharing/app/auth/views.py
@@ -3,16 +3,23 @@ from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from ..models import User
 from .forms import LoginForm
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remeber_me.data)
-            return redirect(request.arg.get('next') or url_for('main.index'))
-        flash('您的邮箱地址或密码有误')
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash(u'您的邮箱地址或密码有误')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash(u'你已经退出当前登录')
+    return redirect(url_for('main.index'))
