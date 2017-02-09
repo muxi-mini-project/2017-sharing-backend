86ae2db05020b3a69e93dfbf8f213e6d32fc8b9b
bofan li <1105388289@qq.com>
2017-02-10 00:05:24 +0800

    第5.1波

--- sharing/app/main/views.py
+++ sharing/app/main/views.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 from flask import render_template, session, redirect, url_for, current_app
 from .. import db
-from ..models import User
+from ..models import User, Post
 from ..email import send_email
 from . import main
 from .forms import NameForm,EditProfileAdminForm ,EditProfileForm, PostForm
@@ -9,23 +9,7 @@ from flask_login import current_user
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                         'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    return "root directory"
 
 @main.route('/user/<username>')
 def user(username):
@@ -53,21 +37,23 @@ def edit_profile():
 
 @main.route('/toshare',methods = ['GET','POST'])
 def toshare():
-	form = PostForm()
-	if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
-		post = Post(body = form.body.data,
-					auther = current_user._get_current_object(),
-					post_type = form.post_type.data)      #分享OR原创
-		db.session.add(post)
-		db.session.commit()
-		return redirect(url_for('.index'))
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
+	post = Post(body = form.body.data,
+		    auther = current_user._get_current_object(),  #获得当前对象
+		    post_type = form.post_type.data)              #分享OR原创
+	db.session.add(post)
+	db.session.commit()
+	return redirect(url_for('.index'))
 
+#去分享的路由
 @main.route('/feed/share',method = ['GET'])
 def post_share():
 	form = PostForm()
 	posts = Post.query.filter_by(post_type=form.post_type.data).filter_by(post_type = 'share').order_by(Post.timestamp.desc()).all()
 	return render_template('share.html',form = form,posts = posts) #分享的文章页面
 
+#博主原创的路由
 @main.route('/feed/original',method = ['GET'])
 def post_original():
 	form = PostForm()
