cea1f7f3ae5a07529e54c285a9d1289be8800ff0
bofan li <1105388289@qq.com>
2017-02-12 01:38:40 +0800

    6.8

--- sharing/app/api_1_0/authentication.py
+++ sharing/app/api_1_0/authentication.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 from flask import g,jsonify
 from flask_httpauth import HTTPBasicAuth
-from ..models import User,AnonymousUser
+from ..models import User, AnonymousUser
 from . import api
 from .errors import unauthorized, forbidden
 
